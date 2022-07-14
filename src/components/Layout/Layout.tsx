130d66be0feeecbe58d4cb704ca241556dbfa97d
Patrick Tan <dygitan@icloud.com>
2022-07-15 01:25:16 +1200

    Add loading indicator on initial load

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,5 @@
-import { Container } from "@mui/material";
+import { useUser } from "@auth0/nextjs-auth0";
+import { Box, CircularProgress, Container, Fade, Typography } from "@mui/material";
 import { ReactNode } from "react";
 import Header from "./Header";
 
@@ -7,14 +8,32 @@ type LayoutProps = {
 };
 
 export default function Layout(props: LayoutProps) {
+  const { isLoading } = useUser();
   return (
     <>
       <Header />
       <main>
         <Container maxWidth="lg" style={{ padding: "50px 0px 0px" }}>
-          {props.children}
+          {isLoading ? <LoadingIndicator /> : props.children}
         </Container>
       </main>
     </>
   );
 }
+
+const LoadingIndicator = () => {
+  return (
+    <Box sx={{ height: 40, textAlign: "center", marginTop: "20%" }}>
+      <Typography style={{ marginBottom: 30 }}>Please wait...</Typography>
+      <Fade
+        in={true}
+        style={{
+          transitionDelay: "0ms",
+        }}
+        unmountOnExit
+      >
+        <CircularProgress />
+      </Fade>
+    </Box>
+  );
+};
